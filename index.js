21da6a0ea512e2db740d418f04290bbf8c89e2ad
Muregz <fabian.murega@student.moringaschool.com>
2024-07-10 21:11:52 +0300

    master

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };  
+  console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])); 
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };  
+  console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  const createFareMultiplier = function(multiplier) {   
+    return function(fare) {
+      return fare * multiplier;
+    };
+  };
+    const fareDoubler = createFareMultiplier(2);
+    const fareTripler = createFareMultiplier(3);
+    const selectDifferentDrivers = function(drivers, selectingFunction) {
+        return selectingFunction(drivers);
+      };
